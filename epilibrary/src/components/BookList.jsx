95f1eae823d5801771d30815ae944cf7838756c3
camillazicari <camyzicari01@gmail.com>
2025-01-13 15:40:07 +0100

    toggle click

--- epilibrary/src/components/BookList.jsx
+++ epilibrary/src/components/BookList.jsx
@@ -1,41 +1,55 @@
-import { Component } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Form, Row } from 'react-bootstrap'
+import { Component } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Form, Row } from "react-bootstrap";
 
 class BookList extends Component {
   state = {
-    searchQuery: '',
-  }
+    searchQuery: "",
+    asin: null,
+  };
+
+  changeState = (asin) => {
+    if (this.state.asin !== asin) {
+      this.setState({ asin: asin });
+    } else {
+      this.setState({ asin: null });
+    }
+  };
 
   render() {
     return (
       <>
-        <Row className="justify-content-center mt-5">
-          <Col xs={12} md={4} className="text-center">
+        <Row className='justify-content-center mt-5'>
+          <Col xs={12} md={4} className='text-center'>
             <Form.Group>
               <Form.Control
-                type="search"
-                placeholder="Cerca un libro"
+                type='search'
+                placeholder='Cerca un libro'
                 value={this.state.searchQuery}
                 onChange={(e) => this.setState({ searchQuery: e.target.value })}
               />
             </Form.Group>
           </Col>
         </Row>
-        <Row className="g-2 mt-3">
+        <Row className='g-2 mt-3'>
           {this.props.books
             .filter((b) =>
               b.title.toLowerCase().includes(this.state.searchQuery)
             )
             .map((b) => (
-              <Col xs={12} md={4} key={b.asin}>
-                <SingleBook book={b} />
+              <Col xs={6} key={b.asin}>
+                <SingleBook
+                  book={b}
+                  asin={this.state.asin === b.asin}
+                  changeState={this.changeState}
+                />
               </Col>
             ))}
+          <Col xs={6}></Col>
         </Row>
       </>
-    )
+    );
   }
 }
 
-export default BookList
+export default BookList;
