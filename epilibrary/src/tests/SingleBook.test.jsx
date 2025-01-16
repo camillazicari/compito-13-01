60d0e0dfc3fdca12ff01f28ce953d0f42d5025ca
camillazicari <camyzicari01@gmail.com>
2025-01-16 17:46:51 +0100

    ES 6

--- epilibrary/src/tests/SingleBook.test.jsx
+++ epilibrary/src/tests/SingleBook.test.jsx
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { render, screen, fireEvent } from "@testing-library/react";
 import SingleBook from "../components/SingleBook";
 import books from "../data/fantasy.json";
+import BookList from "../components/BookList";
 
 describe("onClick behavior", () => {
   it("changes color correctly", () => {
@@ -16,9 +17,16 @@ describe("onClick behavior", () => {
     );
     const card = screen.getByTestId("card");
     const CardClick = () => {
-      fireEvent.click(screen.getByTestId("card"));
+      fireEvent.click(card);
     };
     CardClick();
     expect(card).toHaveProperty(["style", "border"], "3px solid red");
+  });
+  it("changes the color of the second card", () => {
+    render(<BookList books={books} />);
+    const card = screen.getAllByTestId("card");
+    fireEvent.click(card[0]);
+    fireEvent.click(card[1]);
+    expect(card[0]).not.toHaveProperty(["style", "border"], "3px solid red");
   });
 });
