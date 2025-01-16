b89e84d6d53c1ab4c3e024c3157429aa983f2338
camillazicari <camyzicari01@gmail.com>
2025-01-16 15:35:25 +0100

    es 4

--- epilibrary/src/tests/BookList.test.jsx
+++ epilibrary/src/tests/BookList.test.jsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookList from "../components/BookList";
 import books from "../data/fantasy.json";
 
@@ -9,5 +9,16 @@ describe("Test mounting phase", () => {
     const booksJSON = books.length;
     const bookCards = await screen.findAllByTestId("book-cards");
     expect(bookCards).toHaveLength(booksJSON);
+  });
+});
+
+describe("Test searchbar behavior", () => {
+  it("searches correctly", async () => {
+    render(<BookList books={books} />);
+    const bookCardsBefore = await screen.findAllByTestId("book-cards");
+    const searchbar = screen.getByPlaceholderText(/cerca un libro/i);
+    fireEvent.change(searchbar, { target: { value: "amer" } });
+    const bookCardsAfter = await screen.findAllByTestId("book-cards");
+    expect(bookCardsAfter.length).toBeLessThan(bookCardsBefore.length);
   });
 });
